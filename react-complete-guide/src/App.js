b9887b99619ae934b96bd5a91f520d35ff36dc8d
Oleksandr Kiselov <oleksandr.kiselov@sitecore.com>
2021-01-17 02:11:03 +0200

    manipulations with state

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,34 +1,47 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person'
 
-const app = props => {
-  const [ personsState, setPersonsState] = useState({persons: [
-    {name: 'Max', age: 28},
-    {name: 'Lopu', age: 12},
-    {name: 'Ivan', age: 29}
-  ]});
+class App extends Component {
+  state = {
+    persons: [
+      {name: 'Max', age: 28},
+      {name: 'Lopu', age: 12},
+      {name: 'Ivan', age: 29}
+    ]
+  }
 
-  const swithcNameHandler = () =>{
-    setPersonsState({
+  switсhNameHandler = () =>{
+    this.setState({
       persons: [
         {name: 'Max', age: 111128},
         {name: 'Lopu', age: 11112},
         {name: 'Ivan', age: 111129}
       ]
     });
-  };
-  
-  return (
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        {name: 'Max', age: 288},
+        {name: event.target.value, age: 122},
+        {name: 'Ivan', age: 299}
+      ]
+    });
+  }
+
+  render() {
+    return (
       <div className="App">
         <h1>Hi, React)</h1>
-        <button onClick={swithcNameHandler}>Switch Name</button>
-        <Person name={personsState.persons[0].name} age={personsState.persons[0].age}>Racing</Person>
-        <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>Racing</Person>
-        <Person name={personsState.persons[2].name} age={personsState.persons[2].age}>Racing</Person>
+        <button onClick={this.switсhNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>Racing</Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} click={this.switсhNameHandler} changed={this.nameChangedHandler}>Racing</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>Racing</Person>
       </div>
     );
+  }
 }
 
-export default app;
-
+export default App;
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -5,8 +5,9 @@ const person = (props) => {
 
     return (
         <div>
-            <p>I'm a person and I am {age} years {name} old!</p>
+            <p onClick={props.click}>I'm a person and I am {age} years {name} old!</p>
             <p>{props.children}</p>
+            <input type="text" onChange={props.changed}/>
         </div>
     )
 }
